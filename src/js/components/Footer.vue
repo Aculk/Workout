cf8ca449edab2bcb5c0ca54deea974ed4bc7d795
Nikita <nikitahaidar0706@gmail.com>
2024-11-24 20:18:33 +0400

    initial full project

---
+++ src/js/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        <p>Все права защищены &copy</p>
+    </footer>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+    background: #191919;
+    text-align: center;
+    margin-top: 80px;
+    padding: 30px 0;
+    color: #ABABAB;
+    width: 200%;
+    position: relative;
+    left: -50%;
+}
+</style>
